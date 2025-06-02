8d386b414d35d962e5833573771683c48010f3cf
kostya <konstantinmasnikov03@gmail.com>
2025-06-02 23:20:04 +0200

    better blots

--- road_segmentation/plots_drawer/plots_drawer.py
+++ road_segmentation/plots_drawer/plots_drawer.py
@@ -37,6 +37,9 @@ class PlotMetricsCallback(Callback):
             )
             return
 
+        run_name = client.get_run(run_id).data.tags.get("mlflow.runName")
+        self.out_dir = self.out_dir / run_name
+
         train_loss_history = client.get_metric_history(run_id, key="train_loss")
         val_loss_history = client.get_metric_history(run_id, key="val_loss")
         val_f1_history = client.get_metric_history(run_id, key="val_f1")
@@ -47,10 +50,10 @@ class PlotMetricsCallback(Callback):
 
         fig1, ax1 = plt.subplots()
         ax1.plot([m.value for m in train_loss_history], label="train_loss")
-        ax1.plot([m.value for m in val_loss_history], label="val_loss")
-        ax1.set_xlabel("Epoch")
+        # ax1.plot([m.value for m in val_loss_history], label="val_loss")
+        ax1.set_xlabel("Step / 5")
         ax1.set_ylabel("Loss")
-        ax1.set_title("Train vs Validation Loss")
+        ax1.set_title("Train Loss")
         ax1.legend()
         fig1.savefig(self.out_dir / "loss_curve.png")
         plt.close(fig1)
@@ -62,5 +65,14 @@ class PlotMetricsCallback(Callback):
         ax2.set_title("Validation F1 per Epoch")
         fig2.savefig(self.out_dir / "val_f1_curve.png")
         plt.close(fig2)
+
+        fig3, ax3 = plt.subplots()
+        ax3.plot([m.value for m in val_loss_history], label="val_loss")
+        ax3.set_xlabel("Epoch")
+        ax3.set_ylabel("Loss")
+        ax3.set_title("Validation Loss per Epoch")
+        ax3.legend()
+        fig3.savefig(self.out_dir / "val_loss_curve.png")
+        plt.close(fig3)
 
         print(f"[PlotMetricsCallback] Графики сохранены в папке: {self.out_dir}")
