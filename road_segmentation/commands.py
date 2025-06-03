1c01161e46967bc82c0f801cf5454c0ef5d7b7e6
kostya <konstantinmasnikov03@gmail.com>
2025-06-03 23:01:17 +0200

    delete triton_server.py and tensorrt_convert.py

--- road_segmentation/commands.py
+++ road_segmentation/commands.py
@@ -4,7 +4,6 @@ from pathlib import Path
 import fire
 
 from road_segmentation.cli_impl.infer import run_inference
-from road_segmentation.cli_impl.tensorrt_convert import build_engine
 from road_segmentation.cli_impl.train import train_main
 
 
@@ -18,12 +17,6 @@ class Commands:
     def infer(self, onnx: str, image: str, out: str):
         """Run inference using the ONNX model."""
         run_inference(Path(onnx), Path(image), Path(out))
-
-    def tensorrt_convert(
-        self, onnx: str, engine_path: Path, max_batch_size: int = 1, use_fp16: bool = False
-    ):
-        """Convert ONNX model to TensorRT engine."""
-        build_engine(Path(onnx), engine_path, max_batch_size, use_fp16)
 
 
 if __name__ == "__main__":
